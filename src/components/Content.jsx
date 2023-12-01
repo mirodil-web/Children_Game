671bb8b937265804a4cf11b8a98b4735aef89be2
tolamirzayev-077 <mirodiltolamirzayev5@gmail.com>
2023-12-01 12:56:31 +0500

    animals image all add

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,6 +2,14 @@ import Horse from "../../public/assets/horse.gif"
 import Gamebg from "../../public/assets/gamebg.png"
 import I from "../../public/assets/information.png"
 import { useState } from "react"
+import Cat from "../../public/assets/cat.gif"
+import Pet from "../../public/assets/pet.gif"
+import Cow from "../../public/assets/cow.gif"
+import Rabitt from "../../public/assets/rabbit.gif"
+import Chiken from "../../public/assets/chiken.gif"
+
+
+
 
 
 
@@ -14,10 +22,15 @@ const Content = () => {
           <div className="container">
               <div className="border-dashed border-8 max-h-[600px] max-w-[700px] border-teal-500 relative ">
                    <img onClick={()=>setInter(true)} src={I} className="absolute right-3 top-3" width={30} alt="i" />
-                  
+                  <img src={Pet} className="absolute w-[90px] right-80 top-40" alt="cat" />
 
+                <img src={Cat} className="absolute w-[70px] right-40 top-44" alt="cat" />
                   <img className=" w-full h-[400px]   rounded-md" src={Gamebg} alt="bg" />
-                  <img width={150} className="absolute top-40 left-16 z-10" src={Horse} alt="horse" />
+                  <img width={180} className="absolute top-32 left-16 z-10" src={Horse} alt="horse" />
+                  <img className="absolute  left-20 bottom-10" width={80} src={Rabitt} alt="Rabbit" />
+                  <img className="absolute right-20 bottom-16" width={90} src={Cow} alt="Rabbit" />
+                  <img className="absolute top-12 left-[340px]" width={70} src={Chiken} alt="Rabbit" />
+
 
               </div>
           </div>
