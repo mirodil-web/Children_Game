3f1968259983e214c7ce4270bd9b7b5835800470
Zuhriddin1804 <sotvoldiyevzuhriddin52@gmail.com>
2023-12-02 15:18:01 +0500

    The header title has been changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <header className=" py-5 bg-gradient-to-r from-cyan-600 to-violet-500">
             <div className="container flex items-center justify-between">
-                <h1 className="font-bold text-2xl  text-green-300   shadowx">Animals</h1>
+                <h1 className="font-bold text-2xl  text-green-300   shadowx">Hello world</h1>
                 
                 <div className="flex space-x-3 bg-teal-400 rounded-full py-1.5 px-3">
                     <a href="https://t.me/mirodildev/">
